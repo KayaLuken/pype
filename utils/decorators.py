2becb814c8a28fed94c4e1fe6536f506bee75821
KayaLuken <kayaluken@gmail.com>
2021-07-19 22:22:27 +0200

    wrap dummy function in decorators to simulate correct arg count

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,15 +1,26 @@
+from functools import wraps
 
-
-def curry(x, argc=None):
+def curry(f, argc=None):
     if argc is None:
-        argc = x.__code__.co_argcount
+        argc = f.__code__.co_argcount
 
+    def g(x):pass
+    def h(x, y):pass
+    def i(x, y, z):pass
+    def j(x, y, z, v):pass
+    def k(x, y, z, v, w):pass
+
+    m = {1: g, 2: h, 3: i, 4: j, 5: k}
+    w = m[argc]
+
+    # for getting correct arg count of curried function
+    @wraps(w)
     def p(*a):
         if len(a) == argc:
-            return x(*a)
+            return f(*a)
 
         def q(*b):
-            return x(*(a + b))
+            return f(*(a + b))
 
         return curry(q, argc - len(a))
 
