98f46dbd76365e95415947a5b3df6615f7258309
KayaLuken <kayaluken@gmail.com>
2021-07-20 10:26:23 +0200

    make pipe dynamically return function

--- api/pipe.py
+++ api/pipe.py
@@ -2,11 +2,31 @@ from typing import List, Callable
 
 from utils.checkers import check_is_list_of_functions
 from utils.decorators import curry
+from utils.inspectors import arity
+
 
 @curry
-def pipe(fs: List[Callable], x):
+def pipe(fs: List[Callable]):
     check_is_list_of_functions(fs)
-    acc = x
-    for f in fs:
-        acc = f(acc)
-    return acc
+
+    def _f(*x):
+        x0 = fs[0](*x)
+        acc = x0
+        for f in fs[1:]:
+            acc = f(acc)
+        return acc
+
+    def f1(x): return _f(x)
+
+    def f2(x, y): return _f(x, y)
+
+    def f3(x, y, z): return _f(x, y, z)
+
+    def f4(x, y, z, v): return _f(x, y, z, v)
+
+    def f5(x, y, z, v, w): return _f(x, y, z, v, w)
+
+    m = {1: f1, 2: f2, 3: f3, 4: f4, 5: f5}
+    a = arity(fs[0])
+
+    return m[a]
